e91330ffa9ba47ca07774ea5b6a43dee14414f38
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2024-09-26 23:09:31 +0530

    Create setup.py
    just an untested setup.py  trial

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nlp_lib", 
+    version="0.1.0",  
+    author="Shivam Patel",
+    author_email="shivampatel2552003@gmail.com",
+    description="A custom NLP library for Text Pre-processing",
+    long_description=open('README.md').read(),  
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sh1vam/nlp_lib",  
+    packages=find_packages(),  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  
+)
