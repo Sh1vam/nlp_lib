504ad9382d0d3fda46d3cc35fc541882715f39ab
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2024-09-26 23:14:08 +0530

    Update setup.py
    untested setup.py writting trial

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     url="https://github.com/Sh1vam/nlp_lib",  
     packages=find_packages(),  
     classifiers=[
-        "Programming Language :: Python :: 3",  
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',  
