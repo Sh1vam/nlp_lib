6186876e776eb0c1c48fc24113a08090812c5d71
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2024-09-26 23:10:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ setup(
     url="https://github.com/Sh1vam/nlp_lib",  
     packages=find_packages(),  
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  
+        "Programming Language :: Python :: 3",  
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',  
